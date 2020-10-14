0931cdb452f429a5eea8596285ff3fa5d50e4b7f
md-musa <mohammad.musa706@gmail.com>
2020-10-14 22:01:01 +0600

    testing

--- index.js
+++ index.js
@@ -2,95 +2,186 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const {ObjectId} = require("mongodb");
+const fileUpload = require("express-fileupload");
+const fs = require("fs-extra");
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
+app.use(fileUpload());
+
 // ---START------MongoDB connection---------
 const MongoClient = require("mongodb").MongoClient;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iiaf1.mongodb.net/creative-agency?retryWrites=true&w=majority`;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iiaf1.mongodb.net/volunteer-network?retryWrites=true&w=majority`;
-
+const uri =
+  "mongodb+srv://organicUser:quvqc8ro@cluster0.iiaf1.mongodb.net/creative-agency?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 // ---END------MongoDB connection---------
 
 client.connect((err) => {
-  // Volunteer activities collection database
-  const volunteerActivitiesCollection = client
-    .db("volunteer-network")
-    .collection("volunteer-work");
+  console.log("error", err);
+  const ordersCollection = client
+    .db("creative-agency")
+    .collection("all-orders");
+  const reviewsCollection = client
+    .db("creative-agency")
+    .collection("all-reviews");
+  const servicesCollection = client
+    .db("creative-agency")
+    .collection("all-services");
+  const adminCollection = client.db("creative-agency").collection("all-admin");
 
-  // All Event Collection database
-  const eventCollection = client
-    .db("volunteer-network")
-    .collection("event-list");
+  console.log("----MongoDB connected");
+  // ----------start from here-----------------
 
-  console.log("connected with mongoDB");
+  // --------[START]-----Place new order---------------
+  app.post("/subscribeToService", (req, res) => {
+    const file = req.files.file;
+    const {name, email, service, description, price} = req.body;
+    console.log(file, service, description, name, email, price);
 
-  //  [START]-----All event collection send to home---------
-  app.get("/event-collection", (req, res) => {
-    eventCollection.find({}).toArray((err, documents) => {
+    const filePath = `${__dirname}/images/${file.name}`;
+    file.mv(filePath, (err) => {
+      if (err) {
+        res.send({massege: "failed to upload"});
+      }
+      const newImage = fs.readFileSync(filePath);
+      const encImage = newImage.toString("base64");
+      const image = {
+        contentType: file.mimetype,
+        size: file.size,
+        img: Buffer(encImage, "base64"),
+      };
+
+      ordersCollection
+        .insertOne({name, email, service, description, price, image})
+        .then((result) => {
+          fs.remove(filePath, (err) => {
+            if (err) {
+              return res.send({massege: "failed to uploade"});
+            }
+          });
+          console.log(result);
+          res.send(result.insertedCount > 0);
+        });
+    });
+  });
+  // --------[END]-----Place new order---------------
+
+  //---------[START]-----Add review from client----------
+  app.post("/addReview", (req, res) => {
+    reviewsCollection.insertOne(req.body).then((result) => {
+      console.log(result);
+      res.send(result.insertedCount > 0);
+    });
+  });
+  //---------[END]-----Add review from client----------
+
+  //-------[START]--------ADD new service ------------
+  app.post("/addNewService", (req, res) => {
+    const file = req.files.file;
+    const service = req.body.service;
+    const description = req.body.description;
+
+    const filePath = `${__dirname}/images/${file.name}`;
+    file.mv(filePath, (err) => {
+      if (err) {
+        res.send({massege: "failed to upload"});
+      }
+      const newImage = fs.readFileSync(filePath);
+      const encImage = newImage.toString("base64");
+      const image = {
+        contentType: file.mimetype,
+        size: file.size,
+        img: Buffer(encImage, "base64"),
+      };
+
+      servicesCollection
+        .insertOne({service, description, image})
+        .then((result) => {
+          fs.remove(filePath, (err) => {
+            if (err) {
+              return res.send({massege: "failed to uploaded"});
+            }
+          });
+          console.log(result);
+          res.send(result.insertedCount > 0);
+        });
+    });
+  });
+  //-------[END]--------ADD new service ------------
+
+  // -------[START]------Add new admin----------
+  app.post("/addNewAdmin", (req, res) => {
+    console.log(req.body.email);
+    adminCollection.insertOne(req.body).then((result) => {
+      console.log(result);
+      res.send(result.insertedCount > 0);
+    });
+  });
+  // -------[END]------Add new admin----------
+
+  //--------[START]-----All clilent registered services list-------
+  app.get("/allRegisteredServices", (req, res) => {
+    ordersCollection.find({}).toArray((err, documents) => {
       console.log(documents);
       res.send(documents);
     });
   });
-  //  [END]-----All event collection send to home-----------
+  //--------[END]-----All clilent registered services list-------
 
-  // [START]-----Single volunteer joined event list-----------
-  app.get("/get-volunteer-work-list", (req, res) => {
-    volunteerActivitiesCollection
-      .find({email: req.query.email})
-      .toArray((err, documents) => {
-        console.log(err, documents);
-        res.send(documents);
-      });
-  });
-  // [END]-----Single volunteer joined event list-----------
-
-  // [START]-------Delete Single user registered event
-  app.delete("/event-collection/:id", (req, res) => {
-    console.log(req.params.id);
-    volunteerActivitiesCollection
-      .deleteOne({_id: ObjectId(req.params.id)})
-      .then((result) => res.send({deleted: true}));
-  });
-  // [END]-------Delete Single user registered event
-
-  // [START]-------Delete from all registered event
-  app.delete("/admin-delete-registered-volunteer/:id", (req, res) => {
-    console.log(req.params.id);
-    volunteerActivitiesCollection
-      .deleteOne({_id: ObjectId(req.params.id)})
-      .then((result) => res.send({deleted: true}));
-  });
-  // [END]-------Delete from all registered event
-
-  app.post("/add-volunteer-work", (req, res) => {
-    console.log(req.body);
-    volunteerActivitiesCollection.insertOne(req.body).then((result) => {
-      res.send({eventAdded: true});
-    });
-  });
-
-  app.get("/admin/volunteer-register-list", (req, res) => {
-    volunteerActivitiesCollection.find({}).toArray((err, documents) => {
+  app.get("/services", (req, res) => {
+    servicesCollection.find({}).toArray((err, documents) => {
       res.send(documents);
     });
   });
 
-  app.post("/add-event", (req, res) => {
-    console.log(req.body);
-    eventCollection.insertOne(req.body).then((result) => {
-      console.log(result);
-      res.send({isImgSubmitted: true});
+  // get all reviews
+  app.get("/reviews", (req, res) => {
+    reviewsCollection.find({}).toArray((err, documents) => {
+      console.log(documents, err);
+      res.send(documents);
     });
+  });
+
+  //----[START]--Single client registered services
+  app.get("/Single-client-registered-services/:email", (req, res) => {
+    console.log(req.params.email);
+    ordersCollection
+      .find({email: req.params.email})
+      .toArray((err, documents) => {
+        console.log(documents, err);
+        res.send(documents);
+      });
+  });
+  //----[END]--Single client registered services
+
+  //all orders
+  app.get("/orders", (req, res) => {
+    ordersCollection.find({}).toArray((err, documents) => {
+      console.log(documents, err);
+      res.send(documents);
+    });
+  });
+
+  //Upgrade order status like pending, on going , done
+  app.patch("/updateOrderStatus/:id", (req, res) => {
+    ordersCollection
+      .updateOne(
+        {_id: ObjectId(req.params.id)},
+        {
+          $set: {status: req.body.status},
+        }
+      )
+      .then((result) => {
+        console.log(result);
+      });
   });
 });
 
-app.get("/", (req, res) => {
-  res.send({isWorking: true});
-});
-app.listen(process.env.PORT || 5000);
+app.listen(5000);
